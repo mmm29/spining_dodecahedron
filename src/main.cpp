65f3f722d54ccc5610a40b822f0b68c80e505f71
user <>
2022-05-28 13:09:36 +0200

    Refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -104,117 +104,131 @@ static const char dodecahedron_obj[] =
         "f  12  13  11\n"
         "f  19  12  11";
 
-int main() {
+std::unique_ptr<sf::RenderWindow> CreateWindow(const std::string &name, const sf::VideoMode &video_mode) {
     sf::ContextSettings settings;
     settings.depthBits = 12;
     settings.antialiasingLevel = 1;
 
-    sf::VideoMode video_mode(1280, 720);
-    sf::RenderWindow window(video_mode, "Spinning dodecahedron", sf::Style::Default, settings);
+    auto window = std::make_unique<sf::RenderWindow>(video_mode, name, sf::Style::Default, settings);
 
-    if (!window.isOpen()) {
-        printf("Unable to open window.\n");
-        return 1;
-    }
+    if (!window->isOpen())
+        return nullptr;
 
-    window.setVerticalSyncEnabled(true);
-    window.setFramerateLimit(60);
-    window.setMouseCursorVisible(false);
+    window->setVerticalSyncEnabled(true);
+    window->setFramerateLimit(60);
+    window->setMouseCursorVisible(false);
 
-    Menu menu(window);
-    menu.Initialize();
+    return window;
+}
 
-    Engine engine;
-    ViewPort viewport(static_cast<float>(video_mode.width), static_cast<float>(video_mode.height));
+std::unique_ptr<Engine> CreateEngine(sf::RenderWindow *window) {
+    auto engine = std::make_unique<Engine>();
 
     auto sfml_renderer = std::make_shared<render::SFMLRenderer>();
-    sfml_renderer->SetRenderTarget(&window);
+    sfml_renderer->SetRenderTarget(window);
 
     std::shared_ptr<render::Renderer> renderer = sfml_renderer;
 
-    engine.Initialize(viewport, renderer);
+    ViewPort viewport(static_cast<float>(window->getSize().x),
+                      static_cast<float>(window->getSize().y));
 
-    auto camera_controller = std::make_shared<CameraController>();
-    engine.AttachController(camera_controller);
+    engine->Initialize(viewport, renderer);
 
-    sf::Color background_color = sf::Color(0xD7, 0xD7, 0xD7);
+    return engine;
+}
 
-    Menu::DrawData menu_data;
-    menu_data.window_background_color = &background_color;
-    menu_data.engine = &engine;
-    menu_data.cameras["main_camera"] = CameraInfo{
-            .camera = engine.GetActiveCamera()
-    };
-    cameras = &menu_data.cameras; // TODO: remove it
-
-    {
+void InitializeObject(Engine *engine) {
 //        std::string obj_mesh_text = ReadFile("obj/dodecahedron.obj");
 //        assert(!obj_mesh_text.empty() && "Mesh .obj file not found.");
 
-        std::shared_ptr<Mesh> mesh = ObjParser::Parse(dodecahedron_obj, sizeof(dodecahedron_obj) - 1);
-        assert(mesh && "Failed to parse mesh .obj file.");
+    std::shared_ptr<Mesh> mesh = ObjParser::Parse(dodecahedron_obj, sizeof(dodecahedron_obj) - 1);
+    assert(mesh && "Failed to parse mesh .obj file.");
 
-        mesh->Transform(matrix::Scale(3.f));
+    mesh->Transform(matrix::Scale(3.f));
 
-        auto obj = std::make_shared<RigidBody>();
-        obj->SetMesh(mesh);
-        obj->SetColor(Color(0xFF, 0xD3, 0xC9, 0xFF));
-        obj->SetWorldPosition(Vector3(10, 10, 10));
-        obj->SetRotationVelocity(Vector2(M_PI / 2, M_PI / 2));
+    auto obj = std::make_shared<RigidBody>();
+    obj->SetMesh(mesh);
+    obj->SetColor(Color(0xFF, 0xD3, 0xC9, 0xFF));
+    obj->SetWorldPosition(Vector3(10, 10, 10));
+    obj->SetRotationVelocity(Vector2(M_PI / 2, M_PI / 2));
 
-        engine.GetWorld()->AddObject(obj);
-        engine.GetActiveCamera()->AttachTo(obj);
+    engine->GetWorld()->AddObject(obj);
+    engine->GetActiveCamera()->AttachTo(obj);
+}
+
+int main() {
+    std::unique_ptr<sf::RenderWindow> window = CreateWindow("Spinning dodecahedron", sf::VideoMode(1280, 720));
+    if (!window) {
+        printf("Unable to create window.\n");
+        return 1;
     }
+
+    std::unique_ptr<Engine> engine = CreateEngine(window.get());
+    InitializeObject(engine.get());
+
+    auto camera_controller = std::make_shared<CameraController>();
+    engine->AttachController(camera_controller);
+
+    sf::Color background_color = sf::Color(0xD7, 0xD7, 0xD7);
+
+    Menu menu(*window);
+    menu.Initialize();
+
+    Menu::DrawData menu_data;
+    menu_data.window_background_color = &background_color;
+    menu_data.engine = engine.get();
+    menu_data.cameras["main_camera"] = CameraInfo{
+            .camera = engine->GetActiveCamera()
+    };
+    cameras = &menu_data.cameras; // TODO: remove it
 
     sf::Clock delta_clock;
 
-    while (window.isOpen()) {
+    while (window->isOpen()) {
         sf::Time time_elapsed = delta_clock.restart();
 
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (window->pollEvent(event)) {
             menu.ProcessEvent(event);
 
             if (event.type == sf::Event::Closed)
-                window.close();
+                window->close();
             else if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::Key::F2) {
+                    // Menu toggle
                     menu.Toggle();
-                    window.setMouseCursorVisible(menu.IsActive());
+                    window->setMouseCursorVisible(menu.IsActive());
                     if (!menu.IsActive())
-                        SetMouseInCenter(window);
+                        SetMouseInCenter(*window);
                 } else if (event.key.code == sf::Keyboard::Key::Escape)
-                    window.close();
+                    window->close();
             } else if (!menu.IsActive()) {
-                std::shared_ptr<Camera> camera = engine.GetActiveCamera();
-                if (camera) {
-                    if (event.type == sf::Event::MouseMoved) {
-                        sf::Vector2i mouse_position = sf::Mouse::getPosition(window);
-                        sf::Vector2i center_position = GetCenterPosition(window);
-                        if (mouse_position != center_position)
-                            sf::Mouse::setPosition(center_position, window);
+                if (event.type == sf::Event::MouseMoved) {
+                    sf::Vector2i mouse_position = sf::Mouse::getPosition(*window);
+                    sf::Vector2i center_position = GetCenterPosition(*window);
+                    if (mouse_position != center_position)
+                        sf::Mouse::setPosition(center_position, *window);
 
-                        sf::Vector2i mouse_movement = mouse_position - center_position;
-                        camera_controller->HandleMouseMovement(mouse_movement.x, mouse_movement.y);
-                    } else if (event.type == sf::Event::MouseEntered) {
-                        SetMouseInCenter(window);
-                    }
+                    sf::Vector2i mouse_movement = mouse_position - center_position;
+                    camera_controller->HandleMouseMovement(mouse_movement.x, mouse_movement.y);
+                } else if (event.type == sf::Event::MouseEntered) {
+                    SetMouseInCenter(*window);
                 }
             }
         }
 
         // Update controllers
-        engine.Update(time_elapsed.asSeconds());
+        engine->Update(time_elapsed.asSeconds());
         menu.Update(time_elapsed);
 
         // Drawings
-        window.clear(background_color);
-        engine.Draw();
+        window->clear(background_color);
+        engine->Draw();
 
         menu.Draw(&menu_data);
         menu.Render();
 
-        window.display();
+        window->display();
     }
 
     menu.Shutdown();
